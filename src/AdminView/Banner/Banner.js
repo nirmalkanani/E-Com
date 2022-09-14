2b8fefdf74cba47b1fbab681c345bf275d0db7eb
Nirmal Kanani - Code With NK <107502973+nirmalkanani@users.noreply.github.com>
2022-09-15 00:31:28 +0530

    Working on Cateloge Component

---
+++ src/AdminView/Banner/Banner.js
@@ -0,0 +1,16 @@
+export default function Banner() {
+    return (
+        <div className='row banner bg-primary shadow-lg rounded me-3 my-3'>
+            <div className="col-12 d-flex justify-content-between align-items-center">
+                <div className="banner-text py-3">
+                    <p className='m-0 fs-6 text-white'>Big news! We're excited to announce a brand new product.</p>
+                </div>
+                <div className="cls-banner p-3">
+                    <span>
+                        <i class="fa-solid fa-xmark text-white m-0 fs-4"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/AdminView/Cateloge/Cateloge.js
+++ src/AdminView/Cateloge/Cateloge.js
@@ -1,8 +1,9 @@
 import { Select } from 'antd';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Upload } from 'antd';
 import ImgCrop from 'antd-img-crop';
 import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface';
+import Banner from '../Banner/Banner';
 
 const OPTIONS = ["Women", "Men", "Kids", "Jewellery"];
 
@@ -13,59 +14,66 @@ const Cateloge = () => {
 
   const filteredOptions = OPTIONS.filter(o => !selectedItems.includes(o));
 
-    const [fileList, setFileList] = useState([]);
+  const [fileList, setFileList] = useState([]);
 
-    const onChange = ({ fileList: newFileList }) => {
-        setFileList(newFileList);
-        console.log(fileList)
-    };
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+    console.log(fileList)
+  };
 
   return (
-      <div className="container-fluid py-5">
-        <div className="row">
-          <div className="col-12 d-flex justify-content-between align-items-center mb-3">
-            <div>
-              <h4>Add New Product</h4>
-            </div>
-            <div>
-              <p>Learn How to Upload?</p>
-            </div>
-          </div>
-          <hr/>
+    <div className="container-fluid py-5">
+      <form action="#">
+        <div className="row bg-white rounded shadow p-3 mb-3">
           <div className="col-12">
-            <form method="POST">
-              <div className="row">
-        <div className='col-4'>
-          <label className="fs-6 fw-bold mb-2">Select Category</label>
-          <Select
-              mode="multiple"
-              placeholder="Select Product Category"
-              value={selectedItems}
-              onChange={setSelectedItems}
-              style={{ width: '100%' }}
-          >
-            {filteredOptions.map(item => (
-                <Select.Option key={item} value={item}>
-                  {item}
-                </Select.Option>
-            ))}
-          </Select>
-        </div>
-                <div className="col-6">
-                        <Upload
-                            listType="picture-card"
-                            fileList={fileList}
-                            onChange={onChange}
-                            // onPreview={onPreview}
-                        >
-                            {'+ Upload'}
-                        </Upload>
-                </div>
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <h5 className='m-0'>Add New Product</h5>
               </div>
-            </form>
+              <div>
+                <p className='m-0'>Learn to upload catalog?</p>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className='col-4'>
+                <label className="fs-6 fw-bold mb-2">Select Category</label>
+                <Select
+                  mode="multiple"
+                  placeholder="Select Product Category"
+                  value={selectedItems}
+                  onChange={setSelectedItems}
+                  style={{ width: '100%' }}
+                >
+                  {filteredOptions.map(item => (
+                    <Select.Option key={item} value={item}>
+                      {item}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+        {/* Add Rroduct Details */}
+        <div className="row bg-white rounded shadow p-3">
+          <div className="col-8">
+            <h5 className='m-0'>Add Product Details</h5>
+            <Banner />
+          </div>
+          <div className="col-4 border-start border-2 border-dark">
+            <Upload
+              listType="picture-card"
+              fileList={fileList}
+              onChange={onChange}
+            // onPreview={onPreview}
+            >
+              {'+ Upload'}
+            </Upload>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 };
 
