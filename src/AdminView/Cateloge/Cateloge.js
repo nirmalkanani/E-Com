7a2a49c56c3b17f586498675ad87c3cb47a0c49d
Maulik <nirmal.kanani@yahoo.com>
2022-09-15 14:04:41 +0530

    Commit

--- src/AdminView/Cateloge/Cateloge.js
+++ src/AdminView/Cateloge/Cateloge.js
@@ -67,52 +67,100 @@ const Cateloge = () => {
             <hr />
             <div className="row">
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product Name" />
-                  <label for="floatingInput">Product Name</label>
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Name" />
+                  <label for="floatingInput">Product Name <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  {/* Valdation is pending for number */}
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product ID" />
-                  <label for="floatingInput">Style code/ Product ID</label>
+                <div className="form-floating mb-3">
+                  {/* Validation is pending for number */}
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product ID" />
+                  <label for="floatingInput">Style code/ Product ID <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  {/* Valdation is pending for number */}
-                  <select class="form-select">
-                    <option>1</option>
-                  </select>
-                  <label for="floatingInput">GST</label>
+                <div className="form-floating mb-3">
+                  {/* Validation is pending for number */}
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product ID"/>
+                  <label htmlFor="floatingInput">GST <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  {/* Valdation is pending for number */}
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product ID" />
-                  <label for="floatingInput">Style code/ Product ID</label>
+                <div className="form-floating mb-3">
+                  {/* Validation is pending for number */}
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product HSN Code" />
+                  <label for="floatingInput">HSN Code <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  {/* Valdation is pending for number */}
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product Price" />
-                  <label for="floatingInput">Selling Price</label>
+                <div className="form-floating mb-3">
+                  {/* Validation is pending for number */}
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Price" />
+                  <label for="floatingInput">Selling Price <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  {/* Valdation is pending for number */}
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product MRP" />
-                  <label for="floatingInput">MRP</label>
+                <div className="form-floating mb-3">
+                  {/* Validation is pending for number */}
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product MRP" />
+                  <label for="floatingInput">MRP <span className="text-danger">*</span></label>
                 </div>
               </div>
               <div className="col-md-6">
-                <div class="form-floating mb-3">
-                  <input type="text" class="form-control" id="floatingInput" placeholder="Enter Product Name" />
-                  <label for="floatingInput">Product Name</label>
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Size" />
+                  <label for="floatingInput">Size <span className="text-danger">*</span></label>
+                </div>
+              </div>
+            </div>
+            {/*Product Details*/}
+            <div className="title">
+              <span className='fw-bold fs-6 m-0'>Product Details</span>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Color"/>
+                  <label htmlFor="floatingInput">Color</label>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Color"/>
+                  <label htmlFor="floatingInput">Fabric</label>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Product Color"/>
+                  <label htmlFor="floatingInput">COUNTRY OF ORIGIN <span className="text-danger">*</span></label>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Manufacturer Details"/>
+                  <label htmlFor="floatingInput">Manufacturer Details <span className="text-danger">*</span></label>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Enter Packer Details"/>
+                  <label htmlFor="floatingInput">Packer Details <span className="text-danger">*</span></label>
+                </div>
+              </div>
+            </div>
+            {/*Product Details*/}
+            <div className="title">
+              <span className='fw-bold fs-6 m-0'>Product Description</span>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-12">
+                <div className="form-floating">
+                  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"/>
+                  <label htmlFor="floatingTextarea">Product Description <span className="text-danger">*</span></label>
                 </div>
               </div>
             </div>
